3fb947dcceca504ed1af4e0476771b8a70c58ed3
Ryuzaki <iseghiri@gmail.com>
2023-12-05 11:50:34 +0100

    Day 2 1 and 2 success : dirty code

---
+++ day2/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	//FirstStar()
+	SecondStar()
+}
+
+func SecondStar() {
+	input := ReadInput("/day2/input")
+	fileScanner := bufio.NewScanner(input)
+	fileScanner.Split(bufio.ScanLines)
+	result := 0
+	colors := [3]string{"blue", "red", "green"}
+	var patternStrings []string
+	for _, color := range colors {
+		patternStrings = append(patternStrings, fmt.Sprintf("\\d+ %s", color))
+	}
+	pattern := strings.Join(patternStrings, "|")
+	fmt.Println("pattern : ", pattern)
+	re := regexp.MustCompile(pattern)
+	for fileScanner.Scan() {
+		colorsCounter := map[string]int{
+			"blue":  0,
+			"red":   0,
+			"green": 0,
+		}
+		matches := re.FindAllString(fileScanner.Text(), -1)
+		for _, match := range matches {
+			parts := strings.Split(match, " ")
+			number, err := strconv.Atoi(parts[0])
+			check(err)
+			color := parts[1]
+			if number > colorsCounter[color] {
+				colorsCounter[color] = number
+			}
+
+		}
+		product := 1
+		for _, maxNumber := range colorsCounter {
+			product = product * maxNumber
+		}
+		result += product
+
+	}
+	input.Close()
+	println("result : ", result)
+
+}
+
+func FirstStar() {
+	input := ReadInput("/day2/input")
+	fileScanner := bufio.NewScanner(input)
+	fileScanner.Split(bufio.ScanLines)
+	result := 0
+	colors := [3]string{"blue", "red", "green"}
+	var patternStrings []string
+	for _, color := range colors {
+		patternStrings = append(patternStrings, fmt.Sprintf("\\d+ %s", color))
+	}
+	pattern := strings.Join(patternStrings, "|")
+	fmt.Println("pattern : ", pattern)
+	re := regexp.MustCompile(pattern)
+	for fileScanner.Scan() {
+		isValid := true
+		colorsCounter := map[string]int{
+			"blue":  14,
+			"red":   12,
+			"green": 13,
+		}
+		line := fileScanner.Text()
+		gameId := GetGameId(fileScanner.Text())
+		fmt.Println("GameId : ", gameId)
+		matches := re.FindAllString(fileScanner.Text(), -1)
+		fmt.Println("matches", matches)
+		for _, match := range matches {
+			parts := strings.Split(match, " ")
+			number, err := strconv.Atoi(parts[0])
+			check(err)
+			color := parts[1]
+			if number > colorsCounter[color] {
+				isValid = false
+			}
+			if !isValid {
+				fmt.Println("colorsCounter : ", colorsCounter)
+				fmt.Println("(not valid) Line: ", line)
+				break
+			}
+		}
+		if isValid {
+			fmt.Println("colorsCounter : ", colorsCounter)
+			fmt.Println("(valid) Line :", line)
+			result += gameId
+		}
+	}
+	input.Close()
+	println("result : ", result)
+}
+
+func GetGameId(line string) int {
+	gameAndId := strings.Split(strings.Split(line, ":")[0], " ")
+	gameID, err := strconv.Atoi(gameAndId[1])
+	check(err)
+	return gameID
+}
+
+// Helper function to extract keys from a map
+func getKeys(m map[string]int) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func ReadInput(path string) *os.File {
+	pwd, _ := os.Getwd()
+	input, err := os.Open(pwd + path)
+	check(err)
+	return input
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
